5331cf14fee5f8579f5bacc4581de8ab9f995178
Edoardo Balzani <edoardo.balzani87@gmail.com>
2023-08-17 11:41:02 -0400

    bugfixed fit

--- analysis/fit_real_data.py
+++ analysis/fit_real_data.py
@@ -24,6 +24,7 @@ n_basis_funcs = 5
 basis_class_coupling = nsl.basis.RaisedCosineBasisLog
 basis_class_psth = nsl.basis.RaisedCosineBasisLinear
 n_splits = 5
+regularizer_grid = np.logspace(-5, -1, 6)
 
 # define basis
 basis_coupling = basis_class_coupling(n_basis_funcs=n_basis_funcs)
@@ -79,58 +80,24 @@ convolved_spikes = nsl.utils.nan_pad_conv(convolved_spikes, window_size, filter_
 
 del frequency_mask, orientation_mask, orientation_stack, frequency_stack
 
-<<<<<<< HEAD
 # %%
-# fit model
-group_mask = define_groups(n_neurons + n_freq + n_ori, n_basis_funcs=n_basis_funcs)
-# Fit a GLM with group-Lasso
-if len(jax.devices('gpu')):
-    target_device = jax.devices('gpu')[0]
-    y = jax.device_put(y, target_device)
-    X = jax.device_put(X, target_device)
-    group_mask = jax.device_put(group_mask, target_device)
-model = nsl.glm.GLMGroupLasso(
-            solver_kwargs={'tol': 10**-8, 'maxiter': 1000, 'jit':True},
-            inverse_link_function=jnp.exp,alpha=0.05
-)
-model.fit(X,y)
-
-weights_coupling = model.spike_basis_coeff_[:,:n_basis_funcs*n_neurons].reshape(n_neurons, n_neurons, -1)
-filter_predicted = np.einsum("tj, nmj -> nmt", eval_basis_coupling, weights_coupling)
-predicted_coupling_strength = np.linalg.norm(filter_predicted, axis=2)
-
-link = linkage(predicted_coupling_strength, method='average')
-order = leaves_list(link)
-
-fig,_ = plot_coupling_mask(predicted_coupling_strength[order][:,order], title=["Group-Lasso"], cmap="Greys_r", plot_grid=False)
-
-# %%
-# Select regularizer using k-fold
-
-# Pick consecutive blocks, no time point shuffling for time series
-# kf = KFold(n_splits=5, shuffle=False)
-# cls = GridSearchCV(model, param_grid={'alpha': np.logspace(-5, -1, 6)}, cv=kf, verbose=True)
-# cls.fit(X, y, mask = group_mask)
-
-
-# %%
-=======
+# Fit all neurons
 # loop over neurons to use cv and to avoid over-regularizing on low firing rate neurons
 coeffs = np.zeros((n_neurons, (n_neurons + n_freq + n_ori) * n_basis_funcs))
 intercepts = np.zeros((n_neurons,))
 best_alphas = np.zeros((n_neurons, ))
-mean_pr2_test = np.zeros((n_neurons, n_splits))
-std_pr2_test = np.zeros((n_neurons, n_splits))
+mean_pr2_test = np.zeros((n_neurons, regularizer_grid.shape[0]))
+std_pr2_test = np.zeros((n_neurons, regularizer_grid.shape[0]))
 for neu in range(n_neurons):
     print(f"analyzing neuron {neu+1}/{n_neurons}")
     y, X = nsl.utils.combine_inputs(spikes[..., neu: neu+1], jnp.asarray(convolved_spikes), model_ori,
                                   model_freq,strip_left=window_size, reps=1)
 
-    # %%
     # fit model
     group_mask = define_groups(n_neurons + n_freq + n_ori, n_basis_funcs=n_basis_funcs)
     # Fit a GLM with group-Lasso
-    if any("gpu" in device.device_kind.lower() for device in jax.local_devices()):
+    if any("gpu" in device.__repr__().lower() for device in jax.local_devices()):
+        print("Fit with GPU")
         target_device = jax.devices('gpu')[0]
         y = jax.device_put(y, target_device)
         X = jax.device_put(X, target_device)
@@ -141,15 +108,15 @@ for neu in range(n_neurons):
     )
     # Pick consecutive blocks, no time point shuffling for time series
     kf = KFold(n_splits=n_splits, shuffle=False)
-    cls = GridSearchCV(model, param_grid={'alpha': np.logspace(-5, -1, 6)}, cv=kf, verbose=True)
+    cls = GridSearchCV(model, param_grid={'alpha': regularizer_grid}, cv=kf, verbose=True)
     cls.fit(X, y, mask=group_mask)
     model = cls.best_estimator_
-    best_alphas[neu] = cls.best_params_
+    best_alphas[neu] = cls.best_params_['alpha']
     coeffs[neu] = model.spike_basis_coeff_[0]
     intercepts[neu] = model.baseline_log_fr_[0]
     mean_pr2_test[neu] = cls.cv_results_['mean_test_score']
     std_pr2_test[neu] = cls.cv_results_['std_test_score']
-    break
+    
 
 # # save fit results and config
 fhname = f"../results/FitAllNeurons_DT_{int(1000*dt_sec)}ms_NBasis_{n_basis_funcs}_WindowSize_{window_size}.npz"
@@ -166,7 +133,8 @@ np.savez(fhname,
         coeffs = coeffs,
         intercepts = intercepts,
         best_alphas = best_alphas,
-        mean_pr2_test = mean_pr2_test
+        mean_pr2_test = mean_pr2_test,
+        regularizer_grid = regularizer_grid
         )
 
 # # plot the results
@@ -176,4 +144,6 @@ np.savez(fhname,
 # fig,_ = plot_coupling_mask(predicted_coupling_strength, title=["Group-Lasso"], cmap="Greys_r")
 
 
->>>>>>> be327c0a4ae04e9f9c8241398f1b42588257b2b4
+
+
+# %%
